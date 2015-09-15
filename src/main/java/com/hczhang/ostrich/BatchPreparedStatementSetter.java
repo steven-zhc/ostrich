65c7966cc3d9e49b2a3fdfefb0064adbc66f71e8
steven.zhang <haichao.zhang@fuhu.com>
2015-09-15 12:07:35 -0700

    add publish script

--- src/main/java/com/hczhang/ostrich/BatchPreparedStatementSetter.java
+++ src/main/java/com/hczhang/ostrich/BatchPreparedStatementSetter.java
@@ -11,6 +11,7 @@ public interface BatchPreparedStatementSetter {
      * Set parameter values on the given PreparedStatement.
      * @param ps the PreparedStatement to invoke bind methods on
      * @param i index of the statement we're issuing in the batch, starting from 0
+     * @return the values
      */
     BoundStatement setValues(PreparedStatement ps, int i);
 
